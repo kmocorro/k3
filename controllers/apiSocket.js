bf07ca1a8923e801cab88c1836bda071b19eb506
kmocorro <kevinmocorro.github@gmail.com>
2018-05-11 16:19:08 +0800

    weee

--- controllers/apiSocket.js
+++ controllers/apiSocket.js
@@ -1,3 +1,6 @@
+const uuidv1 = require('uuid/v1');
+const moment = require('moment');
+
 module.exports = function(io){
 
     const socket = io('http://localhost:3030', { // Shards server
@@ -26,11 +29,12 @@ module.exports = function(io){
                 console.log('COM path is undefined. Trying to reconnect...');
                 re_connectSerial();
             } else {
+                console.log('Device connected.');
                 socket.emit('device_connected', { 
                     data: { 
                         type: 'Connected', 
                         tool: toolSettings.tool_name,
-                        date_time: new Date()
+                        date_time: moment(new Date()).format()
                     }
                 });
             }
@@ -39,12 +43,9 @@ module.exports = function(io){
             const parser = port.pipe(new ByteLength({ length: 4 })); // Piping data from PLC, plc sends 4 byte data [STX] 2 3 [ETX].
         
             parser.on('data', function(data){ // Parse data from the port.
-
+                //console.log(data);
                 let rawData = data.toString();
                 let rawDataRefined = rawData.slice(1, 3);
-            
-                let signalFromLoader = [];
-                let signalFromUnloader = [];
                 
                 /**
                  *  ===== PLC to SerialPort data param =====
@@ -55,24 +56,28 @@ module.exports = function(io){
                  */
             
                 if(rawDataRefined == 'L0'){ // If data signal is from the Loader,
-                    signalFromLoader.push({
-                        date_time: new Date(),
-                        tool_time: toolSettings.tool_name,
+
+                    let signalFromLoader = {
+                        data_uuid: uuidv1(),
+                        date_time: moment(new Date()).format(),
+                        tool_id: toolSettings.tool_name,
                         param: rawDataRefined,
                         position: 'LOADER',
                         value: 1
-                    });
+                    }
 
                     socket.emit('device_data', signalFromLoader); // Send sinalFromLoader object to Shards Server.
 
                 } else if (rawDataRefined == 'U1'){ // Else if data signal is from the Unloader,
-                    signalFromUnloader.push({
-                        date_time: new Date(),
-                        tool_time: toolSettings.tool_name,
+
+                    let signalFromUnloader = {
+                        data_uuid: uuidv1(),
+                        date_time: moment(new Date()).format(),
+                        tool_id: toolSettings.tool_name,
                         param: rawDataRefined,
                         position: 'UNLOADER',
                         value: 1
-                    });
+                    }
                     
                     socket.emit('device_data', signalFromUnloader); // Send sinalFromUnloader object to Shards Server.
                 }
@@ -80,7 +85,7 @@ module.exports = function(io){
             });
 
         port.on('close', function(){ // port event listener when disconnected invoke reconenct
-            console.log('Disconnected. Trying to reconnect...');
+            console.log('Device disconnected. Trying to reconnect...');
             socket.emit('device_disconnected', { 
                 data: { 
                     type: 'Disconnected', 
